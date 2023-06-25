d8caa2d9ba816ab07725e97e47dbd4dcc1a3f42e
Erik Hedin <erik@erikhedin.com>
2023-06-25 15:43:13 -0700

    Clean up

--- reaction-time.js
+++ reaction-time.js
@@ -1,57 +1,61 @@
-window.addEventListener('DOMContentLoaded', () =>{
- clickTest();
- arduinoTest();
- arduinoTriggerBotTest();
+// Wait for the page to load before binding events.
+window.addEventListener('DOMContentLoaded', () => {
+    clickTest();
+    //  arduinoTest();
+    //  arduinoTriggerBotTest();
 });
 
 const clickTest = () => {
-  const clickArea = document.querySelector('.click-test');
-  const title = clickArea.querySelector('h2');  
-  const message = clickArea.querySelector('.message');
-  let testActive = false;
-  let responseTime = new Date();
+    const clickArea = document.querySelector('.click-test');
+    const message = clickArea.querySelector('.message');
+    let testActive = false;
+    let responseTime = new Date();
 
-  const reactionTest = () => {
-    //Start the test.
-    if (testActive === false) {
-        const testDelay = Math.floor(Math.random() * 3 + 3);
-        message.innerText = 'Test is starting...';
-        //Delay timer to start the test.
-        setTimeout(() => {
-            
-            clickArea.style.backgroundColor = "red";
-            responseTime = new Date();
-            
-            //Activate test
-            testActive = true;
+    const reactionTest = () => {
+        // Test is not running, start the test.
+        if (testActive === false) {
 
-        }, testDelay * 1000);
-    }
+            message.innerText = 'Test is starting...';
+            // Function that the settimeout calls to start the test
+            const startTest = () => {
+                clickArea.style.backgroundColor = "red";
+                // Timestamp in ms.
+                responseTime = new Date();
+                //Activate test state
+                testActive = true;
+            }
 
-    //Finish the test
-    else {
-    
-        const clickTime = new Date();
-        clickArea.style.backgroundColor = "aqua";
-        message.innerText = 'Click anywhere to start the test.';
-        //Finish test
-        testActive = false;
-        //Show results;
-        alert((clickTime - responseTime) + ' ms');
+            // Get an integer between 3000 and 5000 for the set timeout.
+            const testDelay = Math.floor((Math.random() * 3 + 3) * 1000);
+            // Delay timer to start the test. Set timeout is the JS function to create a delay.
+            setTimeout(startTest, testDelay);
+        }
 
-    }
-    
-  };
+        // Handle the user's click to determine the timing and finish the test.
+        else {
+
+            const clickTime = new Date();
+            clickArea.style.backgroundColor = "aqua";
+            message.innerText = 'Click anywhere to start the test.';
+            // Finish test
+            testActive = false;
+            // Show results;
+            alert((clickTime - responseTime) + ' ms');
+
+        }
+
+    };
 
 
-  // Button event to start test.
-  clickArea.addEventListener('click', reactionTest);
+    // Button event to start test.
+    clickArea.addEventListener('click', reactionTest);
 };
 
-const arduinoTest = () => {
+// Hidden for later
+// const arduinoTest = () => {
 
-};
+// };
 
-const arduinoTriggerBotTest = () => {
+// const arduinoTriggerBotTest = () => {
 
-};
+// };
